b4105f43b311a4885d9a8a92ab35801a2441194a
kunal more <mkunal065@gmail.com>
2020-05-28 22:08:15 +0530

    added custom data model & shorten some code.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,17 +11,31 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     //List of Table from Array
-    var itemArray = ["Read a book", "Write article", "List app"]
+   // var itemArray = ["Read a book", "Write article", "List app"]
+    
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+               newItem2.title = "Find "
+               itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+               newItem3.title = " Mike"
+               itemArray.append(newItem3)
+        
         //fetch data from background
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
 
     }
     
@@ -36,7 +50,23 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Did movie the checkMark from didSelectRowAt
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        /*
+         //used Ternary operator instead of below ifelse code
+         if item.done == true {
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }*/
+        
         
         return cell
     }
@@ -45,17 +75,28 @@ class TodoListViewController: UITableViewController {
     //MARK:- TableVieww Deleagate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
+        
+        //writen in single line instead of below ifelse code`
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        /*if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        }else {
+            itemArray[indexPath.row].done = false
+        }*/
+        
+        tableView.reloadData()
         
         //onclick on cell animation
         tableView.deselectRow(at: indexPath, animated: true)
          
         //To Mark and Unmark :CheckMark
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+           /* if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+                tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            }else{
+                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            }*/
 
     }
     
@@ -70,8 +111,11 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //This will happen once the user clicks the Add Item button on our UIAlert
             
+            let newItem = Item()
+            newItem.title = textField.text!
+            
             //adding new list in array
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
             //to store data after terminating app and call than in viewdidload
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
